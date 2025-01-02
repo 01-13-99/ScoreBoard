196e492f31998a20f8ae866825da499ab08319b4
01-13-99 <sofiaeffio@gmail.com>
2025-01-02 13:16:07 -0500

    ScoreBoard

---
+++ index.js
@@ -0,0 +1,47 @@
+let pointsEl = document.getElementById("points-el");
+pointsEl.innerText = 0;
+
+let btnOne = document.getElementById("btn-one");
+let btnTwo = document.getElementById("btn-two");
+let btnThree = document.getElementById("btn-three");
+
+let points = 0;
+
+function plusOneClick(){
+  points +=1
+  pointsEl.innerText = points;
+}
+
+function plusTwoClick(){
+  points +=2
+  pointsEl.innerText = points;
+}
+
+function plusThreeClick(){
+  points +=3
+  pointsEl.innerText = points;
+}
+
+let pointsGuest = document.getElementById("points-guest")
+pointsGuest.innerText = 0;
+
+let btnOneGuest = document.getElementById("btn-one");
+let btnTwoGuest = document.getElementById("btn-two");
+let btnThreeGuest = document.getElementById("btn-three");
+
+guestPoints = 0;
+
+function plusOneClickGuest(){
+  guestPoints +=1
+  pointsGuest.innerText = guestPoints;
+}
+
+function plusTwoClickGuest(){
+  guestPoints +=2
+  pointsGuest.innerText = guestPoints;
+}
+
+function plusThreeClickGuest(){
+  guestPoints +=3
+  pointsGuest.innerText = guestPoints;
+}
